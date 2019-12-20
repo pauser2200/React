50c4a127909b7afdf2274b0fa24f649689024adb
Daniel Yien Han Wu <daniel.wu@cgu.gov.br>
2019-12-20 10:15:48 -0300

    Props

--- components/src/index.js
+++ components/src/index.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from "faker";
 import  CommentDetail from './CommentDetail.js';
-
+import ApprovalCard from './Approvalcard.js';
 
 const App = () =>{
     return (
         <div className="ui container comments">
 
-            <CommentDetail />
-            <CommentDetail  />
-            <CommentDetail  />
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 4:00PM"
+                    avatar={faker.image.avatar()}
+                    content="I like the subject"
+                />
+            </ApprovalCard>
 
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Today at 6:00PM"
+                    avatar={faker.image.avatar()}
+                    content="First blog post!"
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Yesterday 10:00PM"
+                    avatar={faker.image.avatar()}
+                    content="Nice blog post!"
+                />
+            </ApprovalCard>
         </div>
 
     );
