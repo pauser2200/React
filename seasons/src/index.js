e1976330d07c063799617a56832d28cb521eedc3
Daniel Yien Han Wu <daniel.wu@cgu.gov.br>
2019-12-23 13:52:00 -0300

    Componentes States

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,38 +1,42 @@
 import React from 'react';
-import  ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from './Spinner';
 
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
 
-        this.state = { lat: null, erroMessage: '' };
+    state = {lat: null, erroMessage: ''};
+
+    //metodo inicial para instanciar ou inicializar objetos ou regras
+    componentDidMount() {
 
         window.navigator.geolocation.getCurrentPosition(
-            (location) => {
-
-                this.setState({lat: location.coords.latitude, long: location.coords.longitude});
-
-            },
-            (err) => {
-             this.setState({erroMessage: err.message});
-            }
+            position => this.setState({lat: position.coords.latitude}),
+            err => this.setState({erroMessage: err.message})
         );
     }
 
 
-    render() {
+    contentRender() {
 
-        if(this.state.erroMessage && !this.state.lat){
+        if (this.state.erroMessage && !this.state.lat) {
             return <div> Error: {this.state.erroMessage}</div>
         }
 
-        if(!this.state.erroMessage && this.state.lat){
-            return <div> Latitude: {this.state.lat}</div>
+        if (!this.state.erroMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>
         }
 
-        return <div>Loading...</div>
+        return <Spinner message="Please accept location request!"/>
+
+    }
+
+    //Metodo de renderização dos componentes uso obrigatório
+    render() {
+
+        return <div className="border red">{this.contentRender()}</div>
     }
 
 }
