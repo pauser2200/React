33cfaaad3373bcc3ba5b4847483c6dd051101991
Daniel Yien Han Wu <daniel.wu@cgu.gov.br>
2020-01-10 08:19:59 -0300

    Projeto calculadora

--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -9,13 +9,15 @@ import ReactDOM from 'react-dom';
 // import ComponenteComFuncao from './componentes/ComponenteComFuncao'
 // import Pai from './componentes/Pai'
 // import ComponenteClasse from  './componentes/ComponenteClasse'
-import Contador from './componentes/Contador'
+// import Contador from './componentes/Contador'
+import Hook from './componentes/Hook'
 
 const elemento = document.getElementById('root')
 
 ReactDOM.render(
     <div>
-        <Contador numero={0}/>
+        <Hook />
+        {/*<Contador numero={0}/>*/}
         {/*<ComponenteClasse valor="" />*/}
         {/*<Pai />*/}
         {/*<ComponenteComFuncao />*/}
