299f58d5a9b7c736a45394f8740bb50658cb98db
Paulo Sérgio <x71395377120@tcu.gov.br>
2020-02-03 11:49:55 -0300

    Aula 65 consulta e exclusão

--- todoApp/frontend/src/todo/todo.jsx
+++ todoApp/frontend/src/todo/todo.jsx
@@ -1,37 +1,58 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends  Component {
 
     constructor(props){
         super(props)
-        this.state = {description: '', list: []}
+        this.state = { description: '', list: [] }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd= this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+        this.refresh()
     }
 
 
-    handleAddClick(){
-        console.log(this.state.description)
+    handleAdd() {
+        debugger
+        const description = this.state.description
+        axios.post(URL, { description })
+            .then(resp => this.refresh())
     }
 
     handleChange(e){
         this.setState( {...this.state, description: e.target.value })
     }
 
+    refresh() {
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+    }
 
+
+    handleRemove(todo){
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
+    }
 
     render() {
         return (
             <div>
               <PageHeader name='Tarefas ' small='Cadastro'/>
               <TodoForm descripition={this.state.description}
-                        handleAdd={this.handleAddClick.bind(this)}
-                        handleChange={this.handleChange.bind(this)}
+                        handleAdd={this.handleAdd}
+                        handleChange={this.handleChange}
                 />
-              <TodoList />
+              <TodoList list={this.state.list} handleRemove={this.handleRemove} />
             </div>
         )
     }
 }
+
