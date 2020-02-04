5a8a4c969a5eddf00ab183ef0560e6d24990b320
Paulo Sérgio <x71395377120@tcu.gov.br>
2020-02-04 16:04:58 -0300

    Finalizando Projeto TodoApp sem redux

--- todoApp/frontend/src/todo/todo.jsx
+++ todoApp/frontend/src/todo/todo.jsx
@@ -16,12 +16,15 @@ export default class Todo extends  Component {
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd= this.handleAdd.bind(this)
         this.handleRemove = this.handleRemove.bind(this)
+        this.handleMarkAsDone= this.handleMarkAsDone.bind(this)
+        this.handleMarkAsPending= this.handleMarkAsPending.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
+        this.handleClear=this.handleClear.bind(this)
         this.refresh()
     }
 
 
     handleAdd() {
-        debugger
         const description = this.state.description
         axios.post(URL, { description })
             .then(resp => this.refresh())
@@ -31,26 +34,51 @@ export default class Todo extends  Component {
         this.setState( {...this.state, description: e.target.value })
     }
 
-    refresh() {
-        axios.get(`${URL}?sort=-createdAt`)
-            .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+    refresh(description = '') {
+        const search = description ? `&description__regex=/${description}/` : ''
+        axios.get(`${URL}?sort=-createdAt${search}`)
+            .then(resp => this.setState({ ...this.state, description, list: resp.data }))
     }
 
 
     handleRemove(todo){
         axios.delete(`${URL}/${todo._id}`)
-            .then(resp => this.refresh())
+            .then(resp => this.refresh(this.state.description))
+    }
+
+    handleMarkAsDone(todo){
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
+            .then(resp => this.refresh(this.state.description) )
+    }
+    handleMarkAsPending(todo){
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
+            .then(resp => this.refresh(this.state.description) )
+    }
+
+    handleSearch() {
+       this.refresh(this.state.description)
+    }
+
+    handleClear() {
+        debugger
+        this.refresh()
     }
 
     render() {
         return (
             <div>
               <PageHeader name='Tarefas ' small='Cadastro'/>
-              <TodoForm descripition={this.state.description}
+              <TodoForm description={this.state.description}
                         handleAdd={this.handleAdd}
                         handleChange={this.handleChange}
+                        handleSearch={this.handleSearch}
+                        handleClear={this.handleClear.bind(this)}
                 />
-              <TodoList list={this.state.list} handleRemove={this.handleRemove} />
+              <TodoList list={this.state.list}
+                        handleRemove={this.handleRemove}
+                        handleMarkAsDone={this.handleMarkAsDone}
+                        handleMarkAsPending={this.handleMarkAsPending}
+              />
             </div>
         )
     }
--- todoApp/frontend/src/todo/todoForm.jsx
+++ todoApp/frontend/src/todo/todoForm.jsx
@@ -2,20 +2,40 @@ import React  from 'react'
 import Grid from  '../template/grid'
 import IconButton from  '../template/iconButton'
 
-export default props  => (
+export default props  => {
 
-    <div>
-       <div role='form' className='todoForm'>
 
-           <Grid cols='12 9 10'>
-               <input id='description' className='form-control' placeholder='Adcione uma tarefa'
-               value={props.description}
-               onChange={props.handleChange}/>
-           </Grid>
+    const keyHandler = e => {
 
-           <Grid cols='12 3 2'>
-               <IconButton style='primary' onClick={props.handleAdd} icon='plus'/>
-           </Grid>
-       </div>
-    </div>
-)
+        if (e.key === 'Enter'){
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        } else if (e.key === 'Escape') {
+            props.handleClear()
+        }
+
+    }
+
+    return (
+        <div>
+            <div role='form' className='todoForm'>
+
+                <Grid cols='12 9 10'>
+                    <input id='description' className='form-control' placeholder='Adcione uma tarefa'
+                           value={props.description}
+                           onChange={props.handleChange}
+                           onKeyUp={keyHandler}
+                    />
+                </Grid>
+
+                <Grid cols='12 3 2'>
+                    <IconButton style='primary' onClick={props.handleAdd} icon='plus'/>
+                    <IconButton style='info'    onClick={props.handleSearch} icon='search'/>
+                    <IconButton style='default' onClick={props.handleClear} icon='close'/>
+                </Grid>
+            </div>
+        </div>
+    )
+}
+
+
+
