662ad713f4d525d1e88e287c24048cddf4b5a00b
Paulo Sérgio <x71395377120@tcu.gov.br>
2020-02-04 16:29:55 -0300

    Ajustando repositorio

--- exercicios-react/src/componentes/Primeirocomponente.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default (props) =>
-
-    <div>
-        <h1>{props.valor}</h1>
-    </div>
-
-//export default PrimeiroComponente;
--- exercicios-react/src/utils/Utils.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export function componentesFilhosComProps(props) {
-
-    return React.Children.map(props.children, componenteFilho => {
-        return React.cloneElement(componenteFilho, {...props})
-    })
-}
